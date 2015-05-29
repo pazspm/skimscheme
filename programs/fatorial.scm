a17d24605a375a2a6bd4dfeac7472432f552aaf1
Tomer Simis <tls@cin.ufpe.br>
2015-05-29 00:14:58 -0300

    Update fatorial.scm

--- programs/fatorial.scm
+++ programs/fatorial.scm
@@ -1,7 +1,2 @@
-(begin (define fatorial 
-  (lambda (x)
-    (if (lt? x 1) 1 (* x (fatorial (- x 1))))
-   )
-  )
-  (fatorial 10)
-)
+-- Uses if, lt? and recursion
+(begin (define fatorial (lambda (x) (if (lt? x 1) 1 (* x (fatorial (- x 1)))))) (fatorial 10))
