82db2814850088f366387b126991f199b4b9c0d0
Tomer Simis <tls@cin.ufpe.br>
2015-05-29 00:12:45 -0300

    Update OddOrEven.scm

--- programs/OddOrEven.scm
+++ programs/OddOrEven.scm
@@ -1,1 +1,2 @@
+--  Uses comment, lt?, if, mod, div
 (begin (define isEven (lambda (x) (begin (comment (define y (+ x 10))) (if (lt? x 0) (if (eqv? (mod (/ x -1) 2) 0) \"even\" \"odd\" ) (if (eqv? (mod x 2) 0) \"even\" \"odd\" )	)))) (isEven 84))
